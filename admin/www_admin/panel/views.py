96014633eb13460f637a91f6eb4ad1ba12193a90
IWananaI <cristian.parrado.barreto@unillanos.edu.co>
2023-05-12 21:37:00 -0500

    version 0.02 (no funciona el agregar)

--- admin/www_admin/panel/views.py
+++ admin/www_admin/panel/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Usuarios
 # Create your views here.
 
 TEMPLATE_DIRS = (
@@ -10,10 +11,30 @@ def index(request):
     return render(request,"index.html")
 
 def listar(request):
-    return render(request,"crud_usuarios/listar.html")
+    users = Usuarios.objects.all()
+    datos = { 'usuarios' : users }
+    return render(request, "crud_usuarios/listar.html", datos)
 
 def agregar(request):
-    return render(request,"crud_usuarios/agregar.html")
+    if request.method == 'POST':
+        if request.POST.get('pas_per') and request.POST.get('nom_per') and request.POST.get('ape_per') and request.POST.get('tel_per') and request.POST.get('dir_per') and request.POST.get('cat_per') and request.POST.get('vig_per') and request.POST.get('dep_per'):
+            user = Usuarios()
+            user.id_per = request.POST.get('id_per')
+            user.pas_per = request.POST.get('pas_per')
+            user.nom_per = request.POST.get('nom_per')
+            user.ape_per = request.POST.get('ape_per')
+            user.tel_per = request.POST.get('tel_per')
+            user.dir_per = request.POST.get('dir_per')
+            user.cat_per = request.POST.get('cat_per')
+            user.vig_per = request.POST.get('vig_per')
+            user.dep_per = request.POST.get('dep_per')
+            user.save()
+            return redirect('listar')
+
+    else:
+        users = Usuarios.objects.all()
+        datos = {'usuarios' : users}
+        return render(request,"crud_usuarios/agregar.html",datos)
 
 def actualizar(request):
     return render(request,"crud_usuarios/actualizar.html")
